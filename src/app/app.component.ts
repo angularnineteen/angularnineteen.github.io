5dc1a85359460bbc9d89eb19e146bb705ed70f1c
kushal <kushaldeveloper@gmail.com>
2024-12-17 14:27:29 -0500

    dura auto-backup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,6 +18,7 @@ import { RouterOutlet } from '@angular/router';
       h1 {
         color: #2c3e50;
         font-family: Arial, sans-serif;
+        border: 1px solid red;
       }
       #what-is-stale-news {
         font-size: 1.2em;
