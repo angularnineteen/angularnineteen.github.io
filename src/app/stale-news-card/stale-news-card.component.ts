70730e847d9b39306d43b3dae30dd7ac3400c2d0
kushal <kushaldeveloper@gmail.com>
2024-12-17 14:29:59 -0500

    dura auto-backup

---
+++ src/app/stale-news-card/stale-news-card.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-stale-news-card',
+  imports: [],
+  template: `
+    <p>
+      stale-news-card works!
+    </p>
+  `,
+  styles: ``
+})
+export class StaleNewsCardComponent {
+
+}
