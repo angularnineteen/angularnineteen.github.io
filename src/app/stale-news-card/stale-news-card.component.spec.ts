8c6eb487788e13266896f73cecaa38b0431f6085
kushal <kushaldeveloper@gmail.com>
2024-12-17 15:26:53 -0500

    dura auto-backup

--- src/app/stale-news-card/stale-news-card.component.spec.ts
+++ src/app/stale-news-card/stale-news-card.component.spec.ts
@@ -2,6 +2,9 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { StaleNewsCardComponent } from './stale-news-card.component';
 
+import { provideExperimentalZonelessChangeDetection } from '@angular/core';
+import { CommonModule } from '@angular/common'; // Import CommonModule
+
 describe('StaleNewsCardComponent', () => {
   let component: StaleNewsCardComponent;
   let fixture: ComponentFixture<StaleNewsCardComponent>;
@@ -10,7 +13,7 @@ describe('StaleNewsCardComponent', () => {
     await TestBed.configureTestingModule({
       imports: [StaleNewsCardComponent]
     })
-    .compileComponents();
+      .compileComponents();
 
     fixture = TestBed.createComponent(StaleNewsCardComponent);
     component = fixture.componentInstance;
