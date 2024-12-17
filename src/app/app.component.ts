66a977e5c3375eb6a31d8ac9df6665fcf3eb2807
kushal <kushaldeveloper@gmail.com>
2024-12-17 13:25:30 -0500

    dura auto-backup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,13 +7,29 @@ import { RouterOutlet } from '@angular/router';
   template: `
     <h1>Welcome to {{title}}!</h1>
     <p id="what-is-stale-news">
-      We read and forget a lot of things every day. 
-      There is even more we don't get to read at all. 
-      Stale News is here so you can read old news. 
+      We read and forget a lot of things every day.
+      There is even more we don't get to read at all.
+      Stale News is here so you can read old news.
     </p>
-    <router-outlet />
+    <router-outlet></router-outlet>
   `,
-  styles: [],
+  styles: [
+    `
+      h1 {
+        color: #2c3e50;
+        font-family: Arial, sans-serif;
+      }
+      #what-is-stale-news {
+        font-size: 1.2em;
+        color: #34495e;
+        margin: 20px 0;
+      }
+      router-outlet {
+        display: block;
+        margin-top: 20px;
+      }
+    `,
+  ],
 })
 export class AppComponent {
   title = 'Stale News';
