bdca41dbd9072519a04b8763e1873f3ae0c4e921
kushal <kushaldeveloper@gmail.com>
2024-12-18 19:58:56 -0500

    add tests

--- src/app/stale-news-card/stale-news-card.component.spec.ts
+++ src/app/stale-news-card/stale-news-card.component.spec.ts
@@ -1,12 +1,12 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { StaleNewsCardComponent } from './stale-news-card.component';
-
 import { provideExperimentalZonelessChangeDetection } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {AppComponent} from '../app.component';
 
-describe('StaleNewsComponent', () => {
+describe('StaleNewsCardComponent', () => {
+  let component: StaleNewsCardComponent;
+  let fixture: ComponentFixture<StaleNewsCardComponent>;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [CommonModule, StaleNewsCardComponent],
@@ -14,9 +14,45 @@ describe('StaleNewsComponent', () => {
     }).compileComponents();
   });
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+  beforeEach(() => {
+    fixture = TestBed.createComponent(StaleNewsCardComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
   });
+
+  it('should create the component', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should handle empty long form text', async () => {
+    component.longFormText = [];
+    fixture.detectChanges();
+    await fixture.whenStable();
+
+    const compiled = fixture.nativeElement as HTMLElement;
+    const paragraphs = compiled.querySelectorAll('p');
+    expect(paragraphs.length).toBe(5); // Only the static paragraphs should be rendered
+  });
+
+  it('should handle empty title', async () => {
+    component.title = '';
+    await fixture.whenStable();
+
+    const compiled = fixture.nativeElement as HTMLElement;
+    const titles = compiled.querySelectorAll('h2');
+    expect(titles.length).toBe(1);
+    const title = titles[0];
+    expect(title.textContent).toBe('');
+  });
+
+  // it('should handle Lorem Ipsum title', async () => {
+  //   component.title = 'Lorem Ipsum';
+  //   await fixture.whenStable();
+  //
+  //   const compiled = fixture.nativeElement as HTMLElement;
+  //   const titles = compiled.querySelectorAll('h2');
+  //   expect(titles.length).toBe(1);
+  //   const title = titles[0];
+  //   expect(title.textContent).toBe('Lorem Ipsum');
+  // });
 });
