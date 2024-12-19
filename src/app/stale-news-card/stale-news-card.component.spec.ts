5e11e97554594c76bcce1c8d67b7a598845758d2
kushal <kushaldeveloper@gmail.com>
2024-12-18 19:36:43 -0500

    dura auto-backup

--- src/app/stale-news-card/stale-news-card.component.spec.ts
+++ src/app/stale-news-card/stale-news-card.component.spec.ts
@@ -24,50 +24,6 @@ describe('StaleNewsCardComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should render title and subtitle', async () => {
-    component.title = 'Test Title';
-    component.subtitle = 'Test Subtitle';
-    console.error(`hello avocadotoast`);
-    console.error(`hello fsadfasdfasdfasfsafs`);
-    fixture.detectChanges();
-    await fixture.whenStable();
-
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h2')?.textContent).toContain('Test Title');
-    expect(compiled.querySelector('h3')?.textContent).toContain('Test Subtitle');
-  });
-
-  // it('should render authors', async () => {
-  //   component.authors = ['Author One', 'Author Two'];
-  //   fixture.detectChanges();
-  //   await fixture.whenStable();
-  //
-  //   const compiled = fixture.nativeElement as HTMLElement;
-  //   expect(compiled.querySelector('p')?.textContent).toContain('Author One, Author Two');
-  // });
-  //
-  // it('should render canonical URL', async () => {
-  //   component.canonicalUrl = 'https://example.com';
-  //   fixture.detectChanges();
-  //   await fixture.whenStable();
-  //
-  //   const compiled = fixture.nativeElement as HTMLElement;
-  //   const link = compiled.querySelector('a') as HTMLAnchorElement;
-  //   expect(link.href).toBe('https://example.com/');
-  //   expect(link.textContent).toBe('https://example.com');
-  // });
-  //
-  // it('should render long form text', async () => {
-  //   component.longFormText = ['Paragraph 1', 'Paragraph 2'];
-  //   fixture.detectChanges();
-  //   await fixture.whenStable();
-  //
-  //   const compiled = fixture.nativeElement as HTMLElement;
-  //   const paragraphs = compiled.querySelectorAll('p');
-  //   expect(paragraphs[5].textContent).toContain('Item #0: Paragraph 1');
-  //   expect(paragraphs[6].textContent).toContain('Item #1: Paragraph 2');
-  // });
-
   it('should handle empty long form text', async () => {
     component.longFormText = [];
     fixture.detectChanges();
