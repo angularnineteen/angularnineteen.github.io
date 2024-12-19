7ee7dbb87b91e735acebf4a865db60cb56114456
kushal <kushaldeveloper@gmail.com>
2024-12-18 19:53:39 -0500

    dura auto-backup

--- src/app/stale-news-card/stale-news-card.component.spec.ts
+++ src/app/stale-news-card/stale-news-card.component.spec.ts
@@ -53,6 +53,6 @@ describe('StaleNewsCardComponent', () => {
     const titles = compiled.querySelectorAll('h2');
     expect(titles.length).toBe(1);
     const title = titles[0];
-    // expect(title.textContent).toBe('Lorem Ipsum');
+    expect(title.textContent).toBe('Lorem Ipsum');
   });
 });
