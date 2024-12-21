db47273c60f66e7831c90c6b446318bae76ec1b2
kushal <kushaldeveloper@gmail.com>
2024-12-21 08:52:53 -0500

    dura auto-backup

--- src/app/stale-news-card/stale-news-card.component.spec.ts
+++ src/app/stale-news-card/stale-news-card.component.spec.ts
@@ -45,15 +45,15 @@ describe('StaleNewsCardComponent', () => {
     expect(title.textContent).toBe('');
   });
 
-    it('should handle Lorem Ipsum title', async () => {
-      component.title = 'Lorem Ipsum';
-      fixture.detectChanges(); // Trigger Angular's change detection manually
+  it('should handle Lorem Ipsum title', async () => {
+    component.title = 'Lorem Ipsum';
+    fixture.detectChanges(); // Trigger Angular's change detection manually
 
-      const compiled = fixture.nativeElement as HTMLElement;
-      const titles = compiled.querySelectorAll('h2');
-      expect(titles.length).toBe(1);
-      const title = titles[0];
-      expect(title.textContent).toBe('Lorem Ipsum');
-    });
+    const compiled = fixture.nativeElement as HTMLElement;
+    const titles = compiled.querySelectorAll('h2');
+    expect(titles.length).toBe(1);
+    const title = titles[0];
+    expect(title.textContent).toBe('Lorem Ipsum');
+  });
 
 });
